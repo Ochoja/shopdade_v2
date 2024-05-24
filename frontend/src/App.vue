16958d6368bb16b3b7bc24100ae96773f23c06e0
Daniel Ochoja <danielochoja@yahoo.com>
2024-05-24 17:16:46 +0100

    updated

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,3 +5,9 @@ import { RouterView } from 'vue-router'
 <template>
   <RouterView></RouterView>
 </template>
+
+<style lang="postcss">
+.grid-width {
+  @apply w-[95%] ml-auto mr-auto;
+}
+</style>
--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -1,24 +1,56 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
 
 <template>
   <footer class="pt-4 pb-1 bg-primary text-white">
-    <div class="flex justify-center flex-wrap gap-8 mb-6">
+    <div class="grid-width flex justify-between flex-wrap gap-8 mb-16">
       <div>
         <div class="text-2xl font-logo">shopDADE</div>
-        <div class="text-sm max-w-96">
+        <div class="text-sm max-w-[24.5rem]">
           Discover our latest arrivals, curated for the modern working class, shop now to elevate
           your wardrobe with fresh sophistication and style.
         </div>
       </div>
-      <div>
-        <div>Collection</div>
+      <div class="group">
+        <h3>Collection</h3>
+        <RouterLink to="/">Suit</RouterLink>
+        <RouterLink to="/">Footwear</RouterLink>
+        <RouterLink to="/">Accesories</RouterLink>
+        <RouterLink to="/">Tops</RouterLink>
+        <RouterLink to="/">Bottoms</RouterLink>
       </div>
-      <div>
-        <div>Company</div>
+      <div class="group">
+        <h3>Company</h3>
+        <div>Privacy Policy</div>
+        <div>Terms of Service</div>
+        <div>Customer Care</div>
       </div>
-      <div></div>
+      <div class="group">
+        <h3>Social Media</h3>
+      </div>
     </div>
 
-    <div class="text-center text-xs">Copyright ShopDade 2024</div>
+    <div class="text-center">
+      <RouterLink to="/" class="text-xs hover:underline">Copyright ShopDade 2024</RouterLink>
+    </div>
   </footer>
 </template>
+
+<style lang="postcss" scoped>
+.group {
+  @apply flex flex-col gap-1;
+}
+
+.group h3 {
+  @apply text-lg font-bold;
+}
+
+.group div {
+  @apply font-light;
+}
+
+.group a {
+  @apply no-underline;
+}
+</style>
