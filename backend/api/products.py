efd6ae341c6f58d8fc0f9783096074e40b390b2f
Daniel Ochoja <danielochoja@yahoo.com>
2024-06-15 05:14:54 +0100

    added pagination to products route api

--- backend/api/products.py
+++ backend/api/products.py
@@ -9,7 +9,7 @@ product = Blueprint("product", __name__)
 
 
 @product.route("/add/", methods=["POST"])
-def add_item():
+def add_item() -> str:
     """Add item to products collection"""
     item = Product.from_dict(request.json)
     id = mongo.db.products.insert_one(item.to_dict()).inserted_id
@@ -17,30 +17,51 @@ def add_item():
     return f"Item id:{id}"
 
 
+@product.route("/products/")
+def show_products() -> list[dict]:
+    """Returns a list of products based on offset and limit"""
+    offset = request.args.get("offset")  # number of documents to be skipped
+    limit = request.args.get("limit")  # number of items to be displayed
+
+    if limit and not offset:
+        results = mongo.db.products.find().limit(limit)
+        res = [result for result in results]
+        return dumps(res)
+    if offset and limit:
+        results = mongo.db.products.find().skip(offset).limit(limit)
+        res = [result for result in results]
+        return dumps(res)
+    else:
+        results = mongo.db.products.find().limit(10)
+        res = [result for result in results]
+        return dumps(res)
+
+
 @product.route("/search/<query>")
 def find_product(query: str) -> list[dict]:
     """Find one or more products"""
-    print(query)
-    words = query.split("_")
+    offset = request.args.get("offset")
+    words = query.split("-")
     regex_pattern = "|".join([re.escape(word) for word in words])
-    print(regex_pattern)
     query = {
         "$or": [
             {"name": {"$regex": regex_pattern, "$options": "i"}},
             {"description": {"$regex": regex_pattern, "$options": "i"}}
         ]
     }
-    results = mongo.db.products.find(query)
 
-    for result in results:
-        print(result)
-    return jsonify({"result": "successful"})
+    results = mongo.db.products.find(query).offset(offset).limit(
+        10) if offset else mongo.db.products.find(query).limit(10)
+    res = [result for result in results]
+
+    return dumps(res)
 
 
 @product.route("/search/<category>/<query>/")
 def find_by_category(category: str, query: str) -> list[dict]:
     """Find products in a category"""
-    words = query.split("_")
+    offset = request.args.get("offset")
+    words = query.split("-")
     regex_pattern = "|".join([re.escape(word) for word in words])
     query = {
         "$or": [
@@ -50,8 +71,8 @@ def find_by_category(category: str, query: str) -> list[dict]:
         "category": category
     }
 
-    results = mongo.db.products.find(query)
-
+    results = mongo.db.products.find(query).limit(10).offset(
+        offset) if offset else mongo.db.products.find(query).limit(10)
     res = [result for result in results]
 
     return (dumps(res))
